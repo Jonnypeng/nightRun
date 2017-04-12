2f5576123ac7cee91ebb33a920fbdf33201e000b
jonnypeng <jonny.peng@qq.com>
2017-04-12 11:43:37 +0800

    sync

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ function init(){
   var queue = new createjs.LoadQueue();
   queue.installPlugin(createjs.Sound);
   queue.addEventListener('progress',preload);
-  queue.addEventListener('complete',start);
+  queue.addEventListener('complete',menu);
   queue.loadManifest(manifest);
 
   createjs.Ticker.setFPS(24);
@@ -13,47 +13,55 @@ function init(){
   });
 
   function preload(event){
-      console.log(event.progress);
       page0 = new createjs.Container();
-      page0.bg = new createjs.Bitmap(
+      var page0bg = new createjs.Bitmap(
         document.getElementById('background')
       );
-      page0.text = new createjs.Text('Night Run','85px Arial','#eee');
-      page0.text.textAlign = 'center';
-      page0.text.x = 1280 / 2;
-      page0.text.y = 750 / 2  - 150;
-      page0.progress = new createjs.Container();
-      page0.num = new createjs.Text(Math.floor(event.progress*100) + '%','45px Arial','#eee');
-      page0.num.y = 80/2 - 45/2;
-      page0.num.x = 1000/2 - 45/2;
-      page0.proBg = new createjs.Shape();
-      page0.proBg.graphics.beginStroke('#eee').drawRect(0,0,1000,80);
-      page0.proColor = new createjs.Shape();
-      page0.proColor.graphics.beginFill('gray').drawRect(0,0,Math.floor(event.progress*1000),80);
-      page0.progress.addChild(page0.proBg,page0.proColor,page0.num);
-      page0.progress.x = 1280/2 - 1000/2;
-      page0.progress.y = 750/2 - 80/2 + 150;
-      page0.addChild(page0.bg,page0.text,page0.progress);
+      var page0text = new createjs.Text('Night Run','85px Arial','#eee');
+      page0text.textAlign = 'center';
+      page0text.x = 1280 / 2;
+      page0text.y = 750 / 2  - 150;
+      var page0progress = new createjs.Container();
+      console.log(event.progress);
+      var page0num = new createjs.Text(Math.floor(event.progress*10) + '0%','45px Arial','#eee');
+      page0num.y = 80/2 - 45/2;
+      page0num.x = 1000/2 - 45/2;
+      var page0proBg = new createjs.Shape();
+      page0proBg.graphics.beginStroke('#eee').drawRect(0,0,1000,80);
+      var page0proColor = new createjs.Shape();
+      page0proColor.graphics.beginFill('gray').drawRect(0,0,Math.floor(event.progress*1000),80);
+      page0progress.addChild(page0proBg,page0proColor,page0num);
+      page0progress.x = 1280/2 - 1000/2;
+      page0progress.y = 750/2 - 80/2 + 150;
+      page0.addChild(page0bg,page0text,page0progress);
       stage.addChild(page0);
       createjs.Sound.play('GameTheme',{loop:-1});
   };
 
-  function start(){   //载入后游戏准备开始
+  function menu(){   //载入后游戏准备开始
+    page0.removeAllChildren();
+    stage.clear();
     queue.removeEventListener('progress',preload);
-    page0.removeChild(page0.progress)
-    page0.table = new createjs.Bitmap(queue.getResult('backtable'));
-    page0.tableTxt = new createjs.Text('PLAY GAME','36px Arial','#EEE');
-    page0.tableTxt.textAlign = 'center';
+    //page0.removeChild(page0progress,page0num,page0proBg,page0proColor)
+    /*var page0table = new createjs.Bitmap(queue.getResult('backtable'));
+    var page0tableTxt = new createjs.Text('PLAY GAME','36px Arial','#EEE');
+    page0tableTxt.textAlign = 'center';
     createjs.Tween.get(page0.tableTxt,{loop:true}).to({
       'alpha':0
     },500).to({
       'alpha':1
     },500);
-    page0.table.x = 1280/2 - 439/2;
-    page0.table.y = 750/2 - 259/2 + 170;
-    page0.tableTxt.x = 1280 / 2;
-    page0.tableTxt.y = 750 / 2 + 150;
+    page0table.x = 1280/2 - 439/2;
+    page0table.y = 750/2 - 259/2 + 170;
+    page0tableTxt.x = 1280 / 2;
+    page0tableTxt.y = 750 / 2 + 150;
+    page0.addChild(page0table,page0tableTxt);
+    //page0.tableTxt.addEventListener('click',start);*/
+  };
 
-    page0.addChild(page0.table,page0.tableTxt);
+  function start(event){
+    page0.removeAllChildren();
+    console.log(page0);
+    stage.update();
   };
 };
